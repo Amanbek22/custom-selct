2d6d485994dbc86943289243adb08c8434c734f9
unknown <asyl06kair@gmail.com>
2022-05-10 20:04:04 +0600

    feat: added  filter

--- main.js
+++ main.js
@@ -1,112 +1,155 @@
 const dropdownMain = document.querySelectorAll(".dropdown");
-
 const dropdownFunc = (el, dropdownBlock) => {
-  const down = el.querySelector("#down")
+  const down = el.querySelector("#down");
 
-  dropdownBlock.classList.toggle("show")
-  down.classList.toggle("down")
-}
+  dropdownBlock.classList.toggle("show");
+  down.classList.toggle("down");
+};
 
 function onSelect(event, dropdownBlock, arr, selectBtn, dropList) {
   const target = event.target; // Получение элемента на которого мы кликнули
 
   const isInclude = arr.includes(target.innerHTML);
   if (!isInclude) {
-    target.classList.add("selected")
-    arr.push(target.innerHTML)
+    target.classList.add("selected");
+    arr.push(target.innerHTML);
   } else {
-    target.classList.remove("selected")
-    for(let i = 0; i < arr.length; i++) {
-      if(target.innerHTML === arr[i]) {
-        arr[i] = null
+    target.classList.remove("selected");
+    for (let i = 0; i < arr.length; i++) {
+      if (target.innerHTML === arr[i]) {
+        arr[i] = null;
       }
     }
   }
   selectBtn.innerHTML = "Выбрано: " + arr.filter((item) => item).length;
 
-  const closeBtn = document.createElement('img');
+  const closeBtn = document.createElement("img");
   closeBtn.src = "./del.png";
 
   const img = document.createElement("img");
-  img.src = "./down.png"
-  img.id = "down"
+  img.src = "./down.png";
+  img.id = "down";
 
+  selectBtn.append(closeBtn);
 
-
-  selectBtn.append(closeBtn)
-
-  closeBtn.addEventListener("click", function (e) {
+  closeBtn.addEventListener("click", (e) => {
     e.stopPropagation();
     arr.length = [];
-    selectBtn.innerHTML = "Выпадаюший";
+    selectBtn.innerText = "Пол";
 
     const check = dropdownBlock.classList.contains("show");
-    console.log(check);
     if (check) {
-      img.classList.add("down")
+      img.classList.add("down");
     }
-    selectBtn.append(img)
-    dropList.childNodes.forEach(function (item) {
+    selectBtn.append(img);
+    dropList.childNodes.forEach((item) => {
       item.classList?.remove("selected");
-    })
-  })
+    });
+  });
 
-  // Replace if epmty array;
-  if(!arr.filter((i) => i !== null).length) {
+  // Replace if empty array;
+  if (!arr.filter((i) => i !== null).length) {
     selectBtn.innerHTML = "Выпадаюший";
-    img.classList.add("down")
-    selectBtn.append(img)
+    img.classList.add("down");
+    selectBtn.append(img);
   }
 }
 
-
-dropdownMain.forEach(function(el) {
+dropdownMain.forEach((el) => {
   const selectBtn = el.querySelector(".dropbtn");
   const inp = el.querySelector("input");
   const dropdownBlock = el.querySelector("#myDropdown");
-  const dropList = el.querySelector(".drop-list")
+  const dropList = el.querySelector(".drop-list");
 
   const firstDrodown = [];
 
-
   selectBtn.addEventListener("click", () => {
-    dropdownFunc(el, dropdownBlock)
+    dropdownFunc(el, dropdownBlock);
+    fetchData();
   });
 
   inp.addEventListener("keyup", () => {
-    filterFunction(el)
-  })
+    filterFunction(el);
+  });
 
   dropList.addEventListener("click", (e) => {
-    onSelect(e, dropdownBlock, firstDrodown, selectBtn, dropList)
-  })
-
+    onSelect(e, dropdownBlock, firstDrodown, selectBtn, dropList);
+  });
 
   document.addEventListener("click", (e) => {
     if (dropdownBlock.contains(e.target) || selectBtn.contains(e.target)) {
       return null;
     } else {
-      const down = el.querySelector("#down")
+      const down = el.querySelector("#down");
       // Clicked outside the box
-      dropdownBlock.classList.remove("show")
-      down?.classList?.remove("down")
+      dropdownBlock.classList.remove("show");
+      down?.classList?.remove("down");
     }
-  })
-})
+  });
+});
 
+// change input value to current time
+const $currentTime = document.getElementById("todayTime");
+$currentTime.addEventListener("click", () => {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
 
+  let today = year + "-" + month + "-" + day;
+  document.getElementById("second-datePicker").value = today;
+  document.getElementById("first-datePicker").value = today;
+});
 
+// change input value to yesterday's date
+const $yesterdayDate = document.getElementById("yesterday");
+$yesterdayDate.addEventListener("click", () => {
+  let date = new Date();
+  let day = date.getDate() - 1;
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
 
+  let today = year + "-" + month + "-" + day;
+  document.getElementById("first-datePicker").value = today;
+});
 
+// change input value to prev week
+const $prevWeek = document.getElementById("prevWeek");
+$prevWeek.addEventListener("click", () => {
+  let date = new Date();
+  let day = date.getDate() - 7;
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
 
+  let today = year + "-" + month + "-" + day;
+  document.getElementById("second-datePicker").value = today;
+  document.getElementById("first-datePicker").value = today;
+});
 
+// change input value to prev month
+const $prevMonth = document.getElementById("prevMonth");
+$prevMonth.addEventListener("click", () => {
+  let date = new Date();
+  let day = date.getDate() - 1;
+  let month = date.getMonth();
+  let year = date.getFullYear();
 
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
 
-
-
+  let today = year + "-" + month + "-" + day;
+  document.getElementById("second-datePicker").value = today;
+  document.getElementById("first-datePicker").value = today;
+});
 
 //// FILTER
 function filterFunction(el) {
@@ -126,3 +169,32 @@ function filterFunction(el) {
     }
   }
 }
+
+// the appearance of the filter on click
+const $filterBlock = document.querySelector(".filter-block-content");
+const $filterIcon = document.getElementById("filterIcon");
+$filterIcon.addEventListener("click", () => {
+  $filterBlock.classList.toggle("open");
+});
+
+// fetch data for input
+async function fetchData() {
+  const options = document.querySelectorAll("#input-options");
+  let res = await fetch(
+    "https://61dd7484f60e8f0017668817.mockapi.io/input-options"
+  );
+  let data = await res.json();
+  data.map((option, index) => (options[index].innerText = option.option));
+}
+
+// Enums for gender option
+const GENDER_STATES = Object.freeze({
+  MALE: "Male",
+  FEMALE: "Female",
+});
+
+let male = document.querySelector("#gender-male");
+male.innerText = GENDER_STATES.MALE;
+
+let female = document.querySelector("#gender-female");
+female.innerText = GENDER_STATES.FEMALE;
